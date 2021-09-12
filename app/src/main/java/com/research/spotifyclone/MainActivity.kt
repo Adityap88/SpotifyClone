5aab4f320cc8f3b10c289032febab98adfe06af2
Adityap88 <adityapuri8888@gmail.com>
2021-09-12 17:33:07 +0530

    dependency updated

--- app/src/main/java/com/research/spotifyclone/MainActivity.kt
+++ app/src/main/java/com/research/spotifyclone/MainActivity.kt
@@ -8,8 +8,6 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    @Inject
-    lateinit var  glide:RequestManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
